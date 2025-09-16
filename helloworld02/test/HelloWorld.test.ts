63e724232636d7f3b2849a7ad46dffe9dbfccbdb
carlosamcruz <carlosamcruz@ufam.edu.br>
2025-09-16 12:17:19 -0400

    New update

--- helloworld02/test/HelloWorld.test.ts
+++ helloworld02/test/HelloWorld.test.ts
@@ -13,13 +13,13 @@ describe("Hello World", function () {
 
   });
 
-  it("Should read the message", async function () {
+  it("Should be Hello EVM", async function () {
     
     const [owner, otherAccount] = await ethers.getSigners();
 
     const result = await helloWorld.message();
 
-    expect(result === "Hello EVM", "They are not equal");
+    expect(result).to.equal("Hello EVM");
   });
 
   it("Should update message", async function () {
@@ -70,39 +70,3 @@ describe("Hello World", function () {
   });
   
 });
-
-
-/*
-
-describe("Counter", function () {
-  it("Should emit the Increment event when calling the inc() function", async function () {
-    const counter = await ethers.deployContract("Counter");
-
-    await expect(counter.inc()).to.emit(counter, "Increment").withArgs(1n);
-  });
-
-  it("The sum of the Increment events should match the current value", async function () {
-    const counter = await ethers.deployContract("Counter");
-    const deploymentBlockNumber = await ethers.provider.getBlockNumber();
-
-    // run a series of increments
-    for (let i = 1; i <= 10; i++) {
-      await counter.incBy(i);
-    }
-
-    const events = await counter.queryFilter(
-      counter.filters.Increment(),
-      deploymentBlockNumber,
-      "latest",
-    );
-
-    // check that the aggregated events match the current value
-    let total = 0n;
-    for (const event of events) {
-      total += event.args.by;
-    }
-
-    expect(await counter.x()).to.equal(total);
-  });
-});
-*/
